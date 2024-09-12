7bfd6eda884956007c8f2f4f4fcc1f83f8e24ae8
HyukJin98 <dlgurwls4321@gmail.com>
2024-09-12 19:30:52 +0900

    first commit

---
+++ Ex13_JdbcTemplate/src/main/java/com/study/springboot/jdbc/MyUserDTO.java
@@ -0,0 +1,14 @@
+package com.study.springboot.jdbc;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class MyUserDTO {
+
+    private int deptno;
+    private String dname;
+    private String loc;
+        
+}
